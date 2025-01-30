4bbfef5089f59de04b1988a49bdb32f67c99122e
Nachiketh10 <nachiketh1710@gmail.com>
2025-01-30 12:22:30 +0530

    Add files via upload

---
+++ portfolio/portfolio.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for reaching out! I will get back to you shortly.");
+  });
+  
